27f266058da3664d0c58ff719cc12ee18a7c35c7
wd64759 <wei.dong120@gmail.com>
2019-12-16 23:19:17 +0800

    Dec-16 continue updating brick game

--- microbit/blocks.py
+++ microbit/blocks.py
@@ -1,6 +1,7 @@
 import sys
 import random
 
+canvas = [[1,0,0,0,0,0,1],[1,0,0,0,0,0,1],[1,0,0,0,0,0,1],[1,0,0,0,0,0,1],[1,0,0,0,0,0,1],[1,1,1,1,1,1,1]]
 bricks = [[9,9,9,0], [9,9,9,9], [9,0,9,0]]
 rnd_p = random.randint(0, len(bricks) - 1)
 rnd_r = random.randint(0, 3)
@@ -18,11 +19,38 @@ def rotate(brick, times):
     t.extend(brick[0:-times])
     return to_brick(t)
 
-chosen = rotate(bricks[rnd_p], rnd_r)
+def move(brick, loc, mov):
+    to_x, to_y = loc
+    to_x = to_x + mov
+    to_y = to_y + 1 if mov == 0 else to_y
 
-def show(brick, offset):
-    x, y = offset
+    canvas[to_x][to_y]
+
+def mov_brick(brick, loc):
+    to_x, to_y = loc; i = 0; hit = False
+    for bl in brick:
+        overlap = canvas[to_x + i][to_y:to_y + len(bl)]
+        el = 0
+        while el < len(bl):
+            if overlap[el] + bl[el] > 1:
+                hit = True
+                break
+    return hit
+
+def upd_canvas(brick, loc):
+    to_x, to_y = loc; i = 0
+    for bl in brick:
+        canvas[to_x + i][to_y:to_y + len(bl)] = bl
+
+def clean_brick(brick, loc):
+    to_x, to_y = loc
+
+def redraw():
+    pass
     # display.set_pixel(x, y, brick[0])
     # display.set_pixel(x, y + 1, brick[1])
     # display.set_pixel(x + 1, y, brick[2])
     # display.set_pixel(x + 1, y + 1, brick[3])
+
+def main():
+    chosen = rotate(bricks[rnd_p], rnd_r)
